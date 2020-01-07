960389742becac2b431527960f9355b2bdd48072
Craig Bez <bez.craig@gmail.com>
2020-01-07 14:47:09 -0500

    remove city stuff

--- www/city_search.php
+++ www/city_search.php
@@ -15,18 +15,4 @@
     // fwrite( $sqlFile , $sql );
     // fclose( $sqlFile ); 
     // die;
-    header('Content-type: application/json');
-    if( !isset( $_GET['q'] ) || trim( $_GET['q'] ) == "" || strlen( trim( $_GET['q'] ) ) < 3 ){
-        echo json_encode( [] );
-        die;
-    }else{
-        $search = trim( $_GET['q'] ); 
-        $username   ='palletconnect_hud_data';
-        $password   ='Plfxvqk8(iCy'; 
-        $db         ='palletconnect_hud_data'; 
-        $pdo = new PDO("mysql:host=localhost;dbname=$db",$username,$password);
-        $stmt = $pdo->prepare("SELECT * FROM cities WHERE name LIKE ? LIMIT 0,30");
-        $stmt->execute( [ '%' . $search . '%']); 
-        $arr = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        echo( json_encode( $arr ) );
-    }
+     
